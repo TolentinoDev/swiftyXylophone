7c1a7a52c0a3429d7d502b1ae3dac54fdeb2e4bb
Ryan Tolentino <Tolentino2016@gmail.com>
2020-04-16 23:01:19 -0500

    Added button pressed function and added sound assets

--- swiftyXylophone/ViewController.swift
+++ swiftyXylophone/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
+    
+    @IBAction func pressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
 
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+            print("C Pressed")
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
